7f59568f18e68d7d315d9ebe39e236703d3f1b2c
yhwan492 <61440704+yhwan492@users.noreply.github.com>
2022-11-03 20:59:07 -0500

    directory skeleton added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'directory', to: 'directory#index'
   resources :mentor_to_mentees
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
---
+++ spec/views/directory/index.html.erb_spec.rb
@@ -0,0 +1,24 @@
+# require 'rails_helper'
+
+# RSpec.describe "directory/index.html.erb", type: :view do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+# RSpec.describe "directory/index.html.erb", type: :view do
+#   before(:each) do
+#     @users = assign(:directory, User.create!(
+#       mentor_uid: "MyString",
+#       mentee_uid: "MyString",
+#       status: 1,
+#       year: 1
+#     ))
+#   end
+
+#   it "renders users" do
+#     render
+#     expect(rendered).to match(/Mentor Uid/)
+#     expect(rendered).to match(/Mentee Uid/)
+#     expect(rendered).to match(/2/)
+#     expect(rendered).to match(/3/)
+#   end
+# end
