fceff1b4da6f951ad0dbeae47d703a398e57aa47
yhwan492 <61440704+yhwan492@users.noreply.github.com>
2022-11-14 17:09:44 -0600

    add scope on model

--- app/models/group_to_user.rb
+++ app/models/group_to_user.rb
@@ -1,4 +1,7 @@
 class GroupToUser < ApplicationRecord
     validates :uid, presence: true
     validates :group_id, presence: true
+
+    scope :by_group, ->(groupID) { where("group_id = ?", groupID) }
+    scope :by_year, ->(year = "2022") { where("year = ?", year) }
 end
