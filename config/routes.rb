28e48c2e3332680e554e207f88104d8cfd1d02a2
CyrusBuhariwalaTAMU <cyrus.buhariwala@tamu.edu>
2022-11-18 15:50:04 -0600

    about page content

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   get 'directory', to: 'directory#index'
+  get 'admin', to: 'admin#index'
   get 'help', to: 'help#index'
-  get 'admin_help', to: 'admin_help#index'
+  get 'about', to: 'about#index'
+
   resources :mentor_to_mentees
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
@@ -14,8 +17,6 @@ Rails.application.routes.draw do
   resources :users
   root 'welcome#index'
 
-  get 'admin', to: 'admin#index'
-  get 'directory', to: 'directory#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #https://stackoverflow.com/questions/16289299/rails-how-to-destroy-users-created-under-devise
